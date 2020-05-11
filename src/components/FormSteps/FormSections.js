56981b1ed3facdf26b9c22ab6e84481aea66d890
i822610 <da.chambers@sap.com>
2020-05-11 07:59:23 -0700

    FormSections update

--- src/components/FormSteps/FormSections.js
+++ src/components/FormSteps/FormSections.js
@@ -26,28 +26,28 @@ export default class FormSections extends Component {
             );
 
         fetch(this.state.host + "/property")
-        .then(res => res.json())
-        .then(
-            result => {
-                console.log(result);
-                this.setState({
-                    propertytypes: result
-                })
-            }
-        );     
+            .then(res => res.json())
+            .then(
+                result => {
+                    console.log(result);
+                    this.setState({
+                        propertytypes: result
+                    })
+                }
+            );
     }
 
     componentDidUpdate(prevProps) {
         if (prevProps.numberofsections !== this.props.numberofsections) {
             this.setState({
                 numberofsections: this.props.numberofsections
-                
+
             });
         }
         if (prevProps.activesection !== this.props.activesection) {
             this.setState({
                 activesection: this.props.activesection
-                
+
             });
         }
     }
@@ -56,7 +56,7 @@ export default class FormSections extends Component {
         let processedresults = [];
         let keys = Object.keys(result[0]);
         keys.forEach(function (key) {
-            if(typeof result[0][key] === 'object') {
+            if (typeof result[0][key] === 'object') {
                 result[0][key].label = key;
                 if (result[0][key].configurable === "yes") {
                     processedresults.push(result[0][key]);
@@ -66,9 +66,9 @@ export default class FormSections extends Component {
         this.setState({
             //headerprops: this.props.headervalues,
             sectiontemplate: processedresults
-        // }, () => {
-        //     this.processHeaders(this.props.headervalues);
-        // });
+            // }, () => {
+            //     this.processHeaders(this.props.headervalues);
+            // });
         });
     }
 
@@ -77,31 +77,37 @@ export default class FormSections extends Component {
     }
 
     render() {
- 
+
         const { activesection, sectiontemplate } = this.state;
 
         return (
             <div className="formContainer">
-                {"Form Section #" +  (activesection + 1)}
+                {"Form Section #" + (activesection + 1)}
                 <div className="formContent">
-                {sectiontemplate.map(section => (
-                    <div className="createField" key={section.label}>
-                      <TextField className="createLabel" label={"Label for " + section.label} />  
-                      <div className="enableButton">
-                            <Button disableElevation variant="contained" color="primary">Enable</Button>
-                     </div>
-                    </div>
-                ))}
+                    {sectiontemplate.map(section => {
+                        if (section.label === "Name") {
+                            return <div className="createField" key={section.label}>
+                                    <TextField className="createLabel" label={"Label for " + section.label} />
+                                   </div>
+                        } else {
+                            return <div className="createField" key={section.label}>
+                                    <TextField className="createLabel" label={"Label for " + section.label} />
+                                    <div className="enableButton">
+                                        <Button disableElevation variant="contained" color="primary">Enable</Button>
+                                    </div>
+                                   </div>
+                        }
+                    })}
                 </div>
                 <div className="sectionPropertiesToolbar">
                     Selection Properties
                     <div className="toolbarButton">
-                    <Button
-                        variant="outlined"
-                        color="primary"
-                        onClick={this.addPropertyHandler}
-                        disableElevation>
-                        Add New Property
+                        <Button
+                            variant="outlined"
+                            color="primary"
+                            onClick={this.addPropertyHandler}
+                            disableElevation>
+                            Add New Property
                         </Button>
                     </div>
                 </div>
